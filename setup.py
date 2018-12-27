0b79033690e7994a2878690ef565aabc230e5f7a
Mario Kaack <mario@shortrope.com>
2018-12-26 20:17:01 -0800

    hr: human resources initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# with open('README.rst', encoding='UTF-8') as f:
+    # readme = f.read()
+
+setup(
+    name='ht',
+    version='0.1.0',
+    description='Human Resources: Manage users',
+    # long_description=readme,
+    author='Mario',
+    author_email='mario@mario.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[]
+)
